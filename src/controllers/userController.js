4490dba5c21ce15db197a163c67ab2a7edb8a377
jalnarasimha <jalnarasimha@gmail.com>
2026-03-17 19:36:44 +0530

    user create api - working in local

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -36,3 +36,12 @@ exports.createUser = async (req, res) => {
     res.status(500).json({ message: 'Server Error', error: error.message });
   }
 };
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select('-password');
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/register', userController.createUser);
+router.get('/', userController.getAllUsers);
 
 module.exports = router;
